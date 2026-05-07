a9872f434e8a6a9f2a9e68679195e2531af7dac9
Mateus <matt_ufo2@hotmail.com>
2026-05-07 17:58:13 -0300

    feat (inf): implementar estrutura base para migrations e controle de versões
    Adicionada a classe abstrata VersionBase para padronizar a criação de tabelas.
    
    Implementado o método CreateTableWithDefaultColumns para garantir que todas as tabelas possuam as colunas obrigatórias (Id, CreatedAt, Active) por padrão.
    
    Criada a constante DatabaseVersions para centralizar o controle de versionamento das tabelas e facilitar a manutenção das migrations.

---
+++ src/Backend/IveComeToBook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace IveComeToBook.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_USER, "Create table to save user's information")]
+    public class Version0000001 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("Name").AsString(100).NotNullable()
+                .WithColumn("Email").AsString(100).NotNullable()
+                .WithColumn("Password").AsString(2000).NotNullable();
+        }
+    }
+}
---
+++ src/Backend/IveComeToBook.Infrastructure/Migrations/Versions/VersionBase.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using FluentMigrator.Builders.Create.Table;
+
+namespace IveComeToBook.Infrastructure.Migrations.Versions
+{
+    public abstract class VersionBase : ForwardOnlyMigration
+    {
+        protected FluentMigrator.Builders.Create.Table.ICreateTableColumnOptionOrWithColumnSyntax CreateTableWithDefaultColumns(string table)
+        {
+            return Create.Table(table)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("CreatedAt").AsDateTime().NotNullable()
+                .WithColumn("Active").AsBoolean().NotNullable();
+        }
+    }
+}
