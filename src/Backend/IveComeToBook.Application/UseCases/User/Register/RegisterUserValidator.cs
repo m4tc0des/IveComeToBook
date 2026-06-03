c59590b4c2b2e9b116eb4dd0b820cbf0b23b6b47
Mateus <matt_ufo2@hotmail.com>
2026-06-03 12:19:18 -0300

    refact: Refatorado codigo do validator para validação de email.

--- src/Backend/IveComeToBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/IveComeToBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -10,8 +10,11 @@ namespace IveComeToBook.Application.UseCases.User.Register
         {
             RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
             RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
-            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
             RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_TOO_SHORT);
+            When(user => string.IsNullOrWhiteSpace(user.Email) == false, () =>
+            {
+                RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+            });
         }
     }
 }
